e07f5b2a2e5edc8c4cd7bc68e7e55e6655261b9f
Milton Llera <m.lleramontero@bristol.ac.uk>
2019-05-17 14:54:47 +0100

    Fixed missing parameter type
    The parameter `u` needs in `dudt_` when solving stochastic systems needs to be of type `TrackedArray` otherwise the method fails with a `MethodError`.

--- src/Flux/neural_de.jl
+++ src/Flux/neural_de.jl
@@ -21,8 +21,8 @@ neural_msde(x,model,mp,tspan,args...;kwargs...) = neural_msde(x,model,mp,tspan,
                                                          args...;kwargs...)
 function neural_dmsde(model,x,mp,tspan,
                       args...;kwargs...)
-  dudt_(u,p,t) = model(u)
-  g(u,p,t) = mp.*u
+  dudt_(u::TrackedArray,p,t) = model(u)
+  g(u::TrackedArray,p,t) = mp.*u
   prob = SDEProblem(dudt_,g,param(x),tspan,nothing)
   # TODO could probably use vcat rather than collect here
   solve(prob, args...; kwargs...) |> Tracker.collect
